c738b48da2ef993b678b023ea208872f0822d3a6
Luis Ramirez <luislibra90@hotmail.com>
2021-06-19 19:02:10 -0500

    Cambios

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -7,15 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  collapsed: boolean = true;
+  collapsed: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  collapseButton(){
-    this.collapsed = !this.collapsed;
+  colapsa(){
+    
   }
 
+  cambiaClase(){
+    console.log('Cambia clase');
+    let boton = document.getElementById('boton');
+    let div   = document.getElementById('navbarTogglerDemo02');
+    if( boton.classList.length === 1 ){
+      div.classList.remove('collapse');
+      div.classList.remove('show');
+      setTimeout(() => {        
+        boton.classList.add('collapsed');
+        boton.setAttribute('aria-expanded', 'false');
+        div.classList.add('collapsing');
+      }, 2000);
+      setTimeout(() => {
+        div.classList.remove('collapsing');
+        div.classList.add('collapse');
+      }, 2000);
+    }
+  }
 }
